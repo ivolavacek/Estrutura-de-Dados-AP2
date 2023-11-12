8d2be7b26f02ea5d5c269676063392230068cad9
ivolavacek <ivo.lavacek@gmail.com>
2023-11-12 20:45:22 -0300

    gabarito_ap1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Estruturas de Dados - 2023.2
+
+Conteúdo das aulas da disciplina Estruturas de Dados do Ibmec/RJ - Período 2023.2
+
+## Turmas
+
+* [Turma 1](/turma_1/readme.md): segundas, 18h40 às 22h30 (centro)
+* [Turma 2](/turma_2/readme.md): segundas e quartas, 07h30 às 09h20
+* [Turma 3](/turma_3/readme.md): segundas e quartas, 09h50 às 11h40
---
+++ ap1/metricas/metricas.go
@@ -0,0 +1,43 @@
+package metricas
+
+import "fmt"
+
+type Metricas struct {
+	tempoMedioExpedicao float64
+	faturamentoTotal    float64
+	produtosCadastrados int
+	pedidosEncerrados   int
+	pedidosEmAndamento  int
+}
+
+var M = Metricas{
+	tempoMedioExpedicao: 0.0,
+	faturamentoTotal:    0.0,
+	produtosCadastrados: 0,
+	pedidosEncerrados:   0,
+	pedidosEmAndamento:  0,
+}
+
+func (m *Metricas) SomaProdutosCadastrados(valor int) {
+	m.produtosCadastrados += valor
+}
+
+func (m *Metricas) SomaPedidosEmAndamento(valor int) {
+	m.pedidosEmAndamento += valor
+}
+
+func (m *Metricas) AtualizaExpedicao(novoTempo int, valorVenda float64) {
+	tempoTotalExpedicao := m.tempoMedioExpedicao * float64(m.pedidosEncerrados) + float64(novoTempo)
+	m.pedidosEncerrados++
+	m.tempoMedioExpedicao = tempoTotalExpedicao / float64(m.pedidosEncerrados)
+	m.faturamentoTotal += valorVenda
+}
+
+func (m *Metricas) ExibirMetricas() {
+	fmt.Println("\nMétricas de resultados do sistema:")
+	fmt.Println("Número de produtos cadastrados:", m.produtosCadastrados)
+	fmt.Println("Número de pedidos em andamento:", m.pedidosEmAndamento)
+	fmt.Println("Número de pedidos encerrados:", m.pedidosEncerrados)
+	fmt.Printf("Tempo médio para expedição de pedidos (em min): %.2f\n", m.tempoMedioExpedicao)
+	fmt.Printf("Faturamento total: R$ %.2f\n", m.faturamentoTotal)
+}
